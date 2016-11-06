ac41205536658a85c7070faae284412b97ed5a73
Carmen Perez <carmenperez@gmail.com>
2016-11-06 15:12:58 -0500

    added show pages for techniques and tutorials, linked the indexes to the show pages

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -1,4 +1,10 @@
 class TechniquesController < ApplicationController
   def index
+    @techniques = Technique.all
+  end
+
+  def show
+    @technique = Technique.find(params[:id])
+    @tutorials = @technique.tutorials
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :tutorials, only: [:index, :new]
+  resources :tutorials, only: [:index, :new, :show]
   resources :home, only: [:index]
-  resources :techniques, only: [:index]
+  resources :techniques, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
